412fe465f02fa2ef05310c817b5748f6de113f22
aditReza <lete.note3@gmail.com>
2022-05-12 03:20:57 +0700

    implement i18n in hwi

--- src/main/java/com/bltz/rest/webservices/restfulwebservice/Helloworld/HelloWorldController.java
+++ src/main/java/com/bltz/rest/webservices/restfulwebservice/Helloworld/HelloWorldController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
-
 	@Autowired
 	private MessageSource messageSource;
 
@@ -25,5 +27,18 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized(
+			@RequestHeader(name = "Accept-Language", required = false) Locale locale
+	) {
+		System.err.println("_Locale: "+locale);
+		return messageSource.getMessage(
+				"good.morning.message",
+				null,
+				String.format("default message, %s", "good.morning.message"),
+				locale
+		);
 	}
 }
